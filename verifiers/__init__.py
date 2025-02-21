8a974f3b71efc04f8c650fd0cb15f115cb3c9542
stochasm <stochasm2@gmail.com>
2025-02-20 20:22:26 -0700

    forgot to export b64env

--- verifiers/__init__.py
+++ verifiers/__init__.py
@@ -3,6 +3,7 @@ from .envs.code_env import CodeEnv
 from .envs.doublecheck_env import DoubleCheckEnv
 from .envs.math_env import MathEnv
 from .envs.simple_env import SimpleEnv
+from .envs.b64_env import B64Env
 from .utils.data_utils import extract_boxed_answer, extract_hash_answer, preprocess_dataset
 from .utils.model_utils import get_model, get_tokenizer, get_model_and_tokenizer
 from .utils.config_utils import get_default_grpo_config
@@ -19,6 +20,7 @@ __all__ = [
     "DoubleCheckEnv",
     "MathEnv",
     "SimpleEnv",
+    "B64Env",
     "get_model",
     "get_tokenizer",
     "get_model_and_tokenizer",
--- verifiers/envs/__init__.py
+++ verifiers/envs/__init__.py
@@ -5,5 +5,5 @@ from .multistep_env import MultiStepEnv
 from .doublecheck_env import DoubleCheckEnv
 from .code_env import CodeEnv
 from .math_env import MathEnv
-
-__all__ = ['BaseEnv', 'SimpleEnv', 'MultiStepEnv', 'DoubleCheckEnv', 'CodeEnv', 'MathEnv']
+from .b64_env import B64Env
+__all__ = ['BaseEnv', 'SimpleEnv', 'MultiStepEnv', 'DoubleCheckEnv', 'CodeEnv', 'MathEnv', 'B64Env']
